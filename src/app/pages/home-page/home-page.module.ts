0826511564370ea1736e95d7391c92017f437082
abelcabezaroman <contacto@abelcabezaroman.com>
2020-03-02 11:06:25 +0100

    lazy loading

--- src/app/pages/home-page/home-page.module.ts
+++ src/app/pages/home-page/home-page.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HomePageRoutingModule } from './home-page-routing.module';
 import { HomePageComponent } from './home-page.component';
+import { HomePageRoutingModule } from './home-page-routing.module';
 
 
 
