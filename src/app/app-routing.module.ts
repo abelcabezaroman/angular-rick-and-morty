3dbee5f1280be1e992f84fc563b1c809246bff8a
Miguel <miguel17693@hotmail.com>
2020-03-02 12:49:45 +0100

    characters

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,20 +1,32 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import { LocationsPageComponent } from './pages/locations-page/locations-page.component';
-import { CharactersPageComponent } from './pages/characters-page/characters-page.component';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { LocationsPageComponent } from "./pages/locations-page/locations-page.component";
 
-
-const routes: Routes = [{
-  path: '', loadChildren: () => import('./pages/home-page/home-page.module').then(m => m.HomePageModule),
-}, {
-  path: 'characters/:page', component: CharactersPageComponent
-}, {
-  path: 'locations/:page', component: LocationsPageComponent
-}, {
-  path: 'characters', redirectTo: 'characters/1'
-}, {
-  path: 'locations', redirectTo: 'locations/1'
-},
+const routes: Routes = [
+  {
+    path: "",
+    loadChildren: () =>
+      import("./pages/home-page/home-page.module").then(m => m.HomePageModule)
+  },
+  {
+    path: "characters",
+    loadChildren: () =>
+      import("./pages/characters-page/characters-page.module").then(
+        m => m.CharactersPageModule
+      )
+  },
+  {
+    path: "locations/:page",
+    component: LocationsPageComponent
+  },
+  //{
+  //path: "characters",
+  //redirectTo: "characters/1"
+  //},
+  {
+    path: "locations",
+    redirectTo: "locations/1"
+  }
 ];
 
 @NgModule({
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,16 +1,12 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { GalleryComponent } from './components/gallery/gallery.component';
-import { PaginationComponent } from './components/pagination/pagination.component';
-import { MenuComponent } from './components/menu/menu.component';
-
-
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { GalleryComponent } from "./components/gallery/gallery.component";
+import { PaginationComponent } from "./components/pagination/pagination.component";
+import { MenuComponent } from "./components/menu/menu.component";
 
 @NgModule({
   declarations: [GalleryComponent, PaginationComponent, MenuComponent],
-  imports: [
-    CommonModule
-  ],
+  imports: [CommonModule],
   exports: [GalleryComponent, PaginationComponent, MenuComponent]
 })
-export class SharedModule { }
+export class SharedModule {}
