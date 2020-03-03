f2522a0b46a2950dee8788f6cb532e0c9e33cb5a
Catalina Bianchi <catibianchi@hotmail.com>
2020-03-03 15:53:09 +0100

    Subir search form

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { LocationsPageComponent } from './pages/locations-page/locations-page.component';
 
 
 const routes: Routes = [{
