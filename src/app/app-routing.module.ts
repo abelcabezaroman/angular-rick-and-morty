5786a9898a5c210bba1b4fb13c3e3db2f4b4bec8
Abel Cabeza Román <contacto@abelcabezaroman.com>
2020-09-01 16:31:33 +0200

    fix

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { CharactersPageComponent } from "./pages/characters-page/characters-page.component";
 import { HomePageComponent } from "./pages/home-page/home-page.component";
-import { LocationsPageComponent } from "./pages/locations-page/locations-page.component";
 import { CharacterSimplePageComponent } from "./pages/character-simple-page/character-simple-page.component";
 import { LocationsSimplePageComponent } from "./pages/locations-simple-page/locations-simple-page.component";
 
